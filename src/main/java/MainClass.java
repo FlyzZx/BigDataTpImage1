0193aef2e8af33155b360abce163de6d33d81f71
Nicolas Faraci <nicolasfaraci@outlook.com>
2018-10-04 14:08:04 +0200

    Histogramme

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,11 +1,16 @@
+import org.bytedeco.javacpp.indexer.FloatRawIndexer;
+import org.bytedeco.javacpp.indexer.UByteIndexer;
 import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.OpenCVFrameConverter;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
+import static jdk.nashorn.internal.objects.NativeMath.max;
 import static org.bytedeco.javacpp.opencv_core.CV_8U;
-import static org.bytedeco.javacpp.opencv_highgui.waitKey;
+import static org.bytedeco.javacpp.opencv_core.split;
 import static org.bytedeco.javacpp.opencv_imgcodecs.IMREAD_COLOR;
 import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
@@ -20,6 +25,17 @@ public class MainClass {
 
     }
 
+    private void showMat(opencv_core.Mat m) {
+
+        FloatRawIndexer ind = m.createIndexer();
+        for (int rows = 0; rows < m.rows(); rows++) {
+            for (int cols = 0; cols < m.cols(); cols++) {
+                System.out.print(ind.get(rows, cols));
+            }
+            System.out.println("");
+        }
+    }
+
     public static void main(String[] args) {
         opencv_core.Mat image = imread("data/tower.jpg", IMREAD_COLOR);
         if (image == null || image.empty()) {
@@ -30,18 +46,74 @@ public class MainClass {
         Show(image, "original");
 
         //morpho(image);
-        wreckedtomestleseulRGB(image);
+        //wreckedtomestleseulRGB(image);
+        histogramme(image);
 
     }
 
-    private static void wreckedtomestleseulRGB(opencv_core.Mat image) {
+    private static void histogramme(opencv_core.Mat image) {
+        opencv_core.Mat gray = new opencv_core.Mat(image.size());
+        cvtColor(image, gray, CV_RGB2GRAY);
+        Show(gray, "Gray");
 
+        showHistogram(getHistogram(image), "Histogramme");
+    }
+
+    public static void showHistogram(float[] hist, String caption) {
+        int numberOfBins = 256;
+        //	Output	image	size
+        int width = numberOfBins;
+        int height = numberOfBins;
+        //	Set	highest	point	to	90%	of	the	number	of	bins
+        double scale = 1 / max(hist) * height;
+        //	Create	a	color	image	to	draw	on
+        BufferedImage canvas = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = canvas.createGraphics();
+        //	Paint	background
+        g.setPaint(Color.WHITE);
+        g.fillRect(0, 0, width, height);
+        //	Draw	a	vertical	line	for	each	bin
+        g.setPaint(Color.BLUE);
+        for (int bin = 0; bin < numberOfBins; bin++) {
+            int h = (int) Math.round(hist[bin] * scale);
+            g.drawLine(bin, height - 1, bin, height - h - 1);
+        }
+        //	Cleanup
+        g.dispose();
+        //	Create	an	image	and	show	the	histogram
+        CanvasFrame canvasF = new CanvasFrame(caption, 1);
+        canvasF.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        canvasF.showImage(canvas);
+    }
+
+    public static float[] getHistogram(opencv_core.Mat image) {
+        float[] histo = new float[256];
+
+        for (int i = 0; i < histo.length; i++) //INIT RESULT ARRAY TO ZERO
+            histo[i] = 0;
+
+        UByteIndexer indexer = (UByteIndexer) image.createIndexer();
+        for (int x = 0; x < indexer.width() - 1; x++) {
+            for (int y = 0; y < indexer.height() - 1; y++) {
+                int value = indexer.get(y, x);
+                if(value < 256) histo[value]++;
+            }
+        }
+        return histo;
+    }
+
+    private static void wreckedtomestleseulRGB(opencv_core.Mat image) {
+        opencv_core.MatVector rgbSplit = new opencv_core.MatVector();
+        split(image, rgbSplit);
+        Show(rgbSplit.get(0), "Red");
+        Show(rgbSplit.get(1), "Green");
+        Show(rgbSplit.get(2), "Blue");
     }
 
     private static void morpho(opencv_core.Mat image) {
         //Thresh
-        opencv_core.Mat thresh	=	new opencv_core.Mat(image.size());
-        threshold(image,thresh,120,255,THRESH_BINARY_INV);
+        opencv_core.Mat thresh = new opencv_core.Mat(image.size());
+        threshold(image, thresh, 120, 255, THRESH_BINARY_INV);
         Show(thresh, "Thresh");
 
         //Erode
