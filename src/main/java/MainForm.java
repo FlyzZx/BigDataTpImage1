617d0a0534594d0258b9966dda9ef20d05de2c22
Nicolas Faraci <nicolasfaraci@outlook.com>
2018-10-09 11:10:41 +0200

    Préparation UI

---
+++ src/main/java/MainForm.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainForm extends JFrame {
+
+    private JButton lectureEtAffichageDButton;
+    private JButton opérationMorphologiquesEtSeuillagesButton;
+    private JButton canauxDeCouleursButton;
+    private JButton calculDHistogrammeButton;
+    private JButton lookUpTableButton;
+    private JButton segmentationButton;
+    private JPanel jPanel;
+
+    public MainForm() throws HeadlessException {
+        this.add(jPanel);
+    }
+}
