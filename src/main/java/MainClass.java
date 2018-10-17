6e11096ac44542eabeb7b7cc464f8d84f441cb33
thomas.waeters <thomas.waeters@ondulys.com>
2018-10-17 13:39:19 +0200

    Question 8 : Proposer un fonction permettant d'afficher un canal ou un ensemble de canal en couleur pour une image. Fonctionne OK !

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,6 +3,9 @@ import org.bytedeco.javacpp.opencv_core;
 import javax.swing.*;
 import java.awt.*;
 
+import static org.bytedeco.javacpp.opencv_core.CV_8UC1;
+import static org.bytedeco.javacpp.opencv_core.merge;
+import static org.bytedeco.javacpp.opencv_core.split;
 import static org.bytedeco.javacpp.opencv_imgcodecs.IMREAD_COLOR;
 import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
 import static org.bytedeco.javacpp.opencv_imgproc.resize;
@@ -18,5 +21,39 @@ public class MainClass {
         frame.setSize(new Dimension(800, 600));
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private static void splitRGBShow(opencv_core.Mat image, boolean R, boolean G, boolean B){
+        opencv_core.MatVector rgbSplit = new opencv_core.MatVector();
+        opencv_core.MatVector choosenSplit = new opencv_core.MatVector();
+        opencv_core.Mat red = new opencv_core.Mat(image.rows(),image.cols(),CV_8UC1);
+        opencv_core.Mat green = new opencv_core.Mat(image.rows(),image.cols(),CV_8UC1);
+        opencv_core.Mat blue = new opencv_core.Mat(image.rows(),image.cols(),CV_8UC1);
+        opencv_core.Mat result = new opencv_core.Mat();
+        String windowName = "";
+        split(image, rgbSplit);
+
+        if(R){red = rgbSplit.get(2);windowName += "R";}
+        if(G){green = rgbSplit.get(1);windowName += "G";}
+        if(B){blue = rgbSplit.get(0);windowName += "B";}
+
+        choosenSplit.push_back(blue);
+        choosenSplit.push_back(green);
+        choosenSplit.push_back(red);
+        merge(choosenSplit,result);
+        String name = windowName.replaceAll("(?<=.)(?=.)","+");
+        Show(result, name);
+
+        /*
+        Mat cercle = imread("data/Cercle.png",IMREAD_COLOR);
+        splitRGBShow(cercle,true,false,false);
+        splitRGBShow(cercle,false,true,false);
+        splitRGBShow(cercle,false,false,true);
+        splitRGBShow(cercle,true,true,false);
+        splitRGBShow(cercle,false,true,true);
+        splitRGBShow(cercle,true,false,true);
+        splitRGBShow(cercle,true,true,true);
+         */
+
     }
 }
